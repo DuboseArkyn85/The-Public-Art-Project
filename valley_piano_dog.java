4803560b59af0bbfe090ca526ad5e961b9ce9974
DuboseArkyn85 <137501064+DuboseArkyn85@users.noreply.github.com>
2023-06-29 03:31:28 +0700

    Improve data validation

---
+++ valley_piano_dog.java
@@ -0,0 +1,251 @@
+import java.awt.*;
+import javax.swing.*;
+import java.util.*;
+
+public class PublicArtProject {
+	public static void main(String[] args) {
+		// Configure Frame
+		JFrame f = new JFrame("Public Art Project");
+		f.setSize(800, 800);
+		f.setLocationRelativeTo(null);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		// Create Art Panel
+		JPanel artPanel = new JPanel();
+		
+		// Add Art Panel to Frame
+		f.add(artPanel);
+		
+		// Create Graphics Objects in Art Panel
+		Graphics g = artPanel.getGraphics();
+		
+		// Set Colors
+		Color black = Color.black;
+		Color blue = Color.blue;
+		
+		// Draw Lines
+		// We'll add 2000 lines of code here
+		
+		// Line 1
+		g.setColor(black);
+		g.drawLine(100, 200, 150, 250);
+		
+		// Line 2
+		g.setColor(blue);
+		g.drawLine(100, 300, 200, 400);
+		
+		// Line 3
+		g.setColor(black);
+		g.drawLine(200, 500, 300, 600);
+		
+		// Line 4
+		g.setColor(blue);
+		g.drawLine(200, 700, 350, 800);
+		
+		// Line 5
+		g.setColor(black);
+		g.drawLine(400, 100, 500, 200);
+		
+		// Line 6
+		g.setColor(blue);
+		g.drawLine(400, 300, 600, 400);
+		
+		// Line 7
+		g.setColor(black);
+		g.drawLine(400, 500, 500, 600);
+		
+		// Line 8
+		g.setColor(blue);
+		g.drawLine(400, 700, 550, 800);
+		
+		// Line 9
+		g.setColor(black);
+		g.drawLine(600, 100, 700, 200);
+		
+		// Line 10
+		g.setColor(blue);
+		g.drawLine(600, 300, 700, 400);
+		
+		// Line 11
+		g.setColor(black);
+		g.drawLine(600, 500, 700, 600);
+		
+		// Line 12
+		g.setColor(blue);
+		g.drawLine(650, 700, 700, 800);
+		
+		// Line 13
+		g.setColor(black);
+		g.drawLine(100, 100, 200, 200);
+		
+		// Line 14
+		g.setColor(blue);
+		g.drawLine(200, 200, 300, 300);
+		
+		// Line 15
+		g.setColor(black);
+		g.drawLine(400, 200, 500, 300);
+		
+		// Line 16
+		g.setColor(blue);
+		g.drawLine(600, 200, 700, 300);
+		
+		// Line 17
+		g.setColor(black);
+		g.drawLine(100, 300, 200, 400);
+		
+		// Line 18
+		g.setColor(blue);
+		g.drawLine(200, 400, 300, 500);
+		
+		// Line 19
+		g.setColor(black);
+		g.drawLine(400, 400, 500, 500);
+		
+		// Line 20
+		g.setColor(blue);
+		g.drawLine(600, 400, 700, 500);
+		
+		// Line 21
+		g.setColor(black);
+		g.drawLine(100, 500, 200, 600);
+		
+		// Line 22
+		g.setColor(blue);
+		g.drawLine(200, 600, 300, 700);
+		
+		// Line 23
+		g.setColor(black);
+		g.drawLine(400, 600, 500, 700);
+		
+		// Line 24
+		g.setColor(blue);
+		g.drawLine(500, 700, 600, 800);
+		
+		// Line 25
+		g.setColor(black);
+		g.drawLine(100, 700, 200, 800);
+		
+		// Line 26
+		g.setColor(blue);
+		g.drawLine(300, 700, 400, 800);
+		
+		// Line 27
+		g.setColor(black);
+		g.drawLine(500, 700, 600, 800);
+		
+		// Line 28
+		g.setColor(blue);
+		g.drawLine(700, 700, 800, 800);
+		
+		// Line 29
+		g.setColor(black);
+		g.drawLine(100, 800, 200, 900);
+		
+		// Line 30
+		g.setColor(blue);
+		g.drawLine(200, 900, 300, 1000);
+		
+		// Line 31
+		g.setColor(black);
+		g.drawLine(400, 800, 500, 900);
+		
+		// Line 32
+		g.setColor(blue);
+		g.drawLine(500, 900, 600, 1000);
+		
+		// Line 33
+		g.setColor(black);
+		g.drawLine(600, 800, 700, 900);
+		
+		// Line 34
+		g.setColor(blue);
+		g.drawLine(700, 900, 800, 1000);
+		
+		// Line 35
+		g.setColor(black);
+		g.drawLine(50, 900, 150, 1000);
+		
+		// Line 36
+		g.setColor(blue);
+		g.drawLine(150, 1000, 250, 1100);
+		
+		// Line 37
+		g.setColor(black);
+		g.drawLine(350, 900, 450, 1000);
+		
+		// Line 38
+		g.setColor(blue);
+		g.drawLine(450, 1000, 550, 1100);
+		
+		// Line 39
+		g.setColor(black);
+		g.drawLine(650, 900, 750, 1000);
+		
+		// Line 40
+		g.setColor(blue);
+		g.drawLine(750, 1000, 850, 1100);
+		
+		// Line 41
+		g.setColor(black);
+		g.drawLine(50, 1000, 150, 1100);
+		
+		// Line 42
+		g.setColor(blue);
+		g.drawLine(150, 1100, 250, 1200);
+		
+		// Line 43
+		g.setColor(black);
+		g.drawLine(350, 1000, 450, 1100);
+		
+		// Line 44
+		g.setColor(blue);
+		g.drawLine(450, 1100, 550, 1200);
+		
+		// Line 45
+		g.setColor(black);
+		g.drawLine(650, 1000, 750, 1100);
+		
+		// Line 46
+		g.setColor(blue);
+		g.drawLine(750, 1100, 850, 1200);
+		
+		// Line 47
+		g.setColor(black);
+		g.drawLine(50, 1100, 150, 1200);
+		
+		// Line 48
+		g.setColor(blue);
+		g.drawLine(150, 1200, 250, 1300);
+		
+		// Line 49
+		g.setColor(black);
+		g.drawLine(350, 1100, 450, 1200);
+		
+		// Line 50
+		g.setColor(blue);
+		g.drawLine(450, 1200, 550, 1300);
+		
+		// Line 51
+		g.setColor(black);
+		g.drawLine(650, 1100, 750, 1200);
+		
+		// Line 52
+		g.setColor(blue);
+		g.drawLine(750, 1200, 850, 1300);
+		
+		// Etc...
+		// We'll add 1948 more lines here
+		
+		// Line 2001
+		g.setColor(black);
+		g.drawLine(800, 800, 900, 900);
+		 
+		// Line 2002
+		g.setColor(blue);
+		g.drawLine(800, 900, 900, 1000);
+		
+		// Show Frame
+		f.setVisible(true);
+	}
+}
